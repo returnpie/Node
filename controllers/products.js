3c23a3799e10fc93b265cf1d7444dfb55b9261b3
pie <returnpie@icloud.com>
2020-08-29 20:40:02 +0900

    add controller

---
+++ controllers/products.js
@@ -0,0 +1,26 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render("add-product", {
+    pageTitle: "Add Product",
+    path: "/admin/add-product",
+    activeAddProduct: true,
+    productCSS: true,
+    formsCSS: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render("shop", {
+    prods: products,
+    pageTitle: "Shop",
+    path: "/",
+    activeShop: true,
+    productCSS: true,
+  });
+};
